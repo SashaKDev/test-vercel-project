136400da588fb123d30750051cbd27d87e0b19b1
Alexander <sasha.kozel252000@gmail.com>
2025-11-08 21:15:09 +0300

    update setup-app

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,70 +1,15 @@
 import express, {Express, Request, Response} from 'express';
-import {db} from "./db/in-memory.db";
-import {Driver, VehicleFeature} from "./drivers/types/types";
-import {vehicleInputDtoValidation} from "./drivers/validation/vehicleInputDtoValidation";
-import {createErrorMessages} from "./core/utils/error.utils";
+import {driversRouter} from "./drivers/routers/drivers.router";
+import {testingRouter} from "./testing/routers/testing.router";
 
 export const setupApp = (app: Express) => {
 
     app.use(express.json());
 
-    app.get('/', (req: Request, res: Response) => {
-        res
-            .status(200)
-            .json({ message: 'Hello world!' })
-    });
-    app.get('/drivers', (req: Request, res: Response) => {
-        res
-            .status(200)
-            .json(db.drivers);
+    app.get("/", (req: Request, res: Response) => {})
 
-    });
-    app.get('/drivers/:id', (req: Request, res: Response) => {
-        const foundDriver = db.drivers.find(d => d.id === +req.params.id);
-
-        if (!foundDriver) {
-            res.sendStatus(404);
-            return;
-        }
-
-        res
-            .status(200)
-            .json(foundDriver);
-    });
-    app.post('/drivers', (req: Request, res: Response) => {
-
-        const errors = vehicleInputDtoValidation(req.body);
-
-        if (errors.length > 0) {
-            res
-                .status(400)
-                .json(createErrorMessages(errors));
-            return;
-        }
-
-        const newDriver: Driver = {
-            id: db.drivers.length ? db.drivers.length + 1 : 1,
-            name: req.body.name,
-            phoneNumber: req.body.phoneNumber,
-            email: req.body.email,
-            vehicleMake: req.body.vehicleMake, //toyota
-            vehicleModel: req.body.vehicleModel, //camry
-            vehicleYear: req.body.vehicleYear,
-            vehicleLicensePlate: req.body.vehicleLicensePlate,
-            vehicleDescription: req.body.vehicleDescription,
-            vehicleFeatures: req.body.vehicleFeatures,
-            createdAt: new Date()
-        }
-
-        db.drivers.push(newDriver);
-        res
-            .status(201)
-            .json(newDriver);
-    });
-    app.delete('/testing/all-data', (req: Request, res: Response) => {
-        db.drivers = [];
-        res.sendStatus(204);
-    });
+    app.use('/drivers', driversRouter);
+    app.use('/testing', testingRouter);
 
     return app;
 }
