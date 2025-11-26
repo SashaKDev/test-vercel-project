741d1cd38987243d84aff5c8b80590490631a1b0
Alexander <sasha.kozel252000@gmail.com>
2025-11-26 20:21:01 +0300

    commit

--- src/drivers/routers/handlers/get-driver-list.handler.ts
+++ src/drivers/routers/handlers/get-driver-list.handler.ts
@@ -1,7 +1,10 @@
 import {Request, Response} from "express";
 import {db} from "../../../db/in-memory.db";
+import {Driver} from "../../types/types";
+import {driversRepository} from "../../repositories/drivers.repository";
 
 
 export const getDriverListHandler = (req: Request, res: Response) => {
-    res.status(200).json(db.drivers);
+    const foundDrivers: Driver[] = driversRepository.findAll();
+    res.status(200).json(foundDrivers);
 }
